6d8a44bc8b04778aa6c145f6a9fb2fdd3dbd834d
Kurt P <krprudhomme@gmail.com>
2013-07-20 12:23:20 -0500

    Move verbose and sources new ~/.bashrc

--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@
 ##This is just a little script to cp all of the *.bak files over to the 
 ##/home/$USER/ directory. I don't know if it'll work. I'll have to test it out.
 
+echo "Staring copy"
 cp bashrc.bak ~/.bashrc
 cp gitconfig.bak ~/.gitconfig
 cp vimrc.bak ~/.vimrc
+echo "Finished copy"
 
+echo "Sourcing ~/.bashrc"
+source ~/.bashrc
+echo "Done!"
