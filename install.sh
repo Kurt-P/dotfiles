0582c4a8c719ef0b7e0032c82afda993678e9448
Kurt P <krprudhomme@gmail.com>
2013-07-20 12:09:22 -0500

    Added an small install script. See `install.sh`

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+##This is just a little script to cp all of the *.bak files over to the 
+##/home/$USER/ directory. I don't know if it'll work. I'll have to test it out.
+
+cp bashrc.bak ~/.bashrc
+cp gitconfig.bak ~/.gitconfig
+cp vimrc.bak ~/.vimrc
+
